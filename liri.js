c8350046b80214c904c12757e1d6e94dc2b396db
Eleanor Mark <emark@Eleanors-MBP.attlocal.net>
2017-04-11 02:42:44 -0700

    added do-what-it-says feature

--- liri.js
+++ liri.js
@@ -6,16 +6,55 @@ var Twitter = require('twitter');
 var spotify = require('spotify');
 var fs = require('fs');
 
-var inputCategory = process.argv[2]; 
+var command = process.argv[2]; 
+var title = process.argv.slice(3).join(' ');
 
-if (inputCategory === 'my-tweets') {
+function getMovie() {
+    var movie = title  || 'Mr Nobody';
+    var _movieName = title.split(' ').join('_') || 'Mr_Nobody';
+    var movieInfo;
+    request('http://www.omdbapi.com/?t=' + movie, function (error, response, body) {
+        if (!error) {
+            movieInfo = JSON.parse(body);
+            console.log('Movie Title: ' + movieInfo['Title'] +
+                '\n* Release Date: ' + movieInfo['Released'] +
+                '\n* IMDB Rating: ' + movieInfo['imdbRating'] +
+                '\n* Productin Country: ' + movieInfo['Country'] +
+                '\n* Language: ' + movieInfo['Language'] +
+                '\n* Plot: ' + movieInfo['Plot'] +
+                '\n* Actors: ' + movieInfo['Actors'] +
+                '\n* Rotton Tomatoes Ratings: ' + movieInfo['Ratings'][1]['Value'] +
+                '\n* Website: ' + movieInfo['Website'] +
+                '\n* Rotten Tomatoes URL: ' + 'https://www.rottentomatoes.com/m/' + _movieName + '/' 
+                );
+        } else {
+            console.log('error:', error); // Print the error if one occurred 
+        }
+    }); 
+}
 
+function getSong() {
+
+    var song = title  || 'dancing in the moonlight';
+    spotify.search({ type: 'track', query: song }, function(err, data) {
+        if ( err ) {
+            console.log('Error occurred: ' + err);
+            return;
+        }
+        console.log("* Song Name: " + data.tracks.items[0].name);
+        console.log("* Artist Name: " + data.tracks.items[0].artists[0].name);
+        console.log("* Preview: " + data.tracks.items[0].preview_url);
+        console.log("* Album Name: " + data.tracks.items[0].album.name);
+    });
+}
+
+function getTweets() {
     var twitterClient = new Twitter(keys.twitterKeys);
     var  Twitter_Params = {screen_name: 'eleanormark', count: 20}
 
     twitterClient.get('statuses/user_timeline', Twitter_Params, function(error, tweets, response) {
         if (!error) {
-            tweets.forEach(function (tweet) { console.log("Tweet: '" + tweet.text + 
+            tweets.forEach(function (tweet) { console.log("* Tweet: '" + tweet.text + 
                 "' was created " + tweet.created_at);
             });
         } else {
@@ -23,42 +62,29 @@ if (inputCategory === 'my-tweets') {
         }
     });
 }
-if (inputCategory === 'spotify-this-song') {
 
-    var song = process.argv.slice(3).join(' ') || 'dancing in the moonlight';
-    spotify.search({ type: 'track', query: song }, function(err, data) {
-        if ( err ) {
-            console.log('Error occurred: ' + err);
-            return;
-        }
+function getRandom() {
+    fs.readFile("random.txt", "utf-8", function(err, data) {
+        if (err) console.log("Error: " + err);
 
-        console.log("Song Name: " + data.tracks.items[0].name);
-        console.log("Artist Name: " + data.tracks.items[0].artists[0].name);
-        console.log("Preview: " + data.tracks.items[0].preview_url);
-        console.log("Album Name: " + data.tracks.items[0].album.name);
-
-    });
+        command = data.split(",")[0];
+        title = data.split(",")[1] || "Yellow Submarine";
+        console.log(command + " " + title);
+        runCommand();
+	});
 }
-if (inputCategory === 'movie-this') {
 
-    var movie = process.argv.slice(3).join(' ') || 'Mr Nobody';
-    var _movieName = process.argv.slice(3).join('_') || 'Mr_Nobody';
-    var movieInfo;
-    request('http://www.omdbapi.com/?t=' + movie, function (error, response, body) {
-        if (!error) {
-            movieInfo = JSON.parse(body);
-            console.log('Movie Title: ' + movieInfo['Title'] +
-                '\nRelease Date: ' + movieInfo['Released'] +
-                '\nIMDB Rating: ' + movieInfo['imdbRating'] +
-                '\nProductin Country: ' + movieInfo['Country'] +
-                '\nLanguage: ' + movieInfo['Language'] +
-                '\nPlot: ' + movieInfo['Plot'] +
-                '\nActors: ' + movieInfo['Actors'] +
-                '\nRotton Tomatoes Ratings: ' + movieInfo['Ratings'][1]['Value'] +
-                '\nWebsite: ' + movieInfo['Website'] +
-                '\nRotten Tomatoes URL: ' + 'https://www.rottentomatoes.com/m/' + _movieName + '/' );
-        } else {
-            console.log('error:', error); // Print the error if one occurred 
-        }
-    });  
+function runCommand() {
+    if (command === 'my-tweets') {
+        getTweets();
+    } else if (command === 'spotify-this-song') {
+        getSong();
+    } else if (command === 'movie-this') {
+        getMovie(); 
+    } else if (command === "do-what-it-says") {
+        getRandom();
+        }   
 }
+
+runCommand();
+
